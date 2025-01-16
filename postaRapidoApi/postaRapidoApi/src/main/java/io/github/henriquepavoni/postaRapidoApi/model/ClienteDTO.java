e0ca3275e3c98a394912915bcec96bdb9912ae5e
Windows10 <HenriquePavoni>
2025-01-16 17:10:53 -0300

    Adicionado model do cliente e 'ClienteDTO'.

---
+++ postaRapidoApi/postaRapidoApi/src/main/java/io/github/henriquepavoni/postaRapidoApi/model/ClienteDTO.java
@@ -0,0 +1,39 @@
+package io.github.henriquepavoni.postaRapidoApi.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public record ClienteDTO(
+        @NotEmpty
+        String nome,
+
+        @Email
+        String email,
+
+        String telefone,
+
+        @CPF
+        String cpf,
+
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        Date dataNascimento,
+
+        String endereco,
+
+        String cidade,
+
+        String estado,
+
+        String cep,
+
+        LocalDateTime dataCadastro,
+
+        boolean ativo ){
+
+}
