d7ec47bcff6adb894dbe86844809ec7d0d396990
Windows10 <HenriquePavoni>
2025-01-16 16:32:36 -0300

    Criado projeto, inicializado no github e iniciado desenvolvimento do Back-End.

---
+++ postaRapidoApi/postaRapidoApi/src/main/java/io/github/henriquepavoni/postaRapidoApi/PostaRapidoApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.henriquepavoni.postaRapidoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostaRapidoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostaRapidoApiApplication.class, args);
+	}
+
+}
---
+++ postaRapidoApi/postaRapidoApi/src/test/java/io/github/henriquepavoni/postaRapidoApi/PostaRapidoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.henriquepavoni.postaRapidoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostaRapidoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
