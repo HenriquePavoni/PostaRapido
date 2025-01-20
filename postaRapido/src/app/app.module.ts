c68c68110d1e743c3ec244411be8e5a399b28f42
Windows10 <HenriquePavoni>
2025-01-20 16:02:00 -0300

    Realizado ajustes na API e criado formulário de cliente.

--- postaRapido/src/app/app.module.ts
+++ postaRapido/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component';
+import { ClientesModule } from './clientes/clientes.module';
+import { ClientesService } from './clientes.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,14 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    TemplateModule
+    TemplateModule,
+    ClientesModule
   ],
-  providers: [],
+  providers: [
+    ClientesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
