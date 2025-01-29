fb09eb1908a361dc45e4ad579121837ba61326fb
Windows10 <HenriquePavoni>
2025-01-28 22:46:49 -0300

    Realizado adição do update de clientes no projeto e realizado ajustes na API.

--- postaRapido/src/app/clientes.service.ts
+++ postaRapido/src/app/clientes.service.ts
@@ -10,11 +10,20 @@ export class ClientesService {
 
   constructor(private http: HttpClient) { }
 
-  salvar( cliente: Cliente) : Observable<Cliente> {
+  salvar(cliente: Cliente): Observable<Cliente> {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
   }
 
-  getAllClientes() : Observable<Cliente[]> {
+  getAllClientes(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
   }
+
+  getClienteById(id: number): Observable<Cliente> {
+    return this.http.get<Cliente>(`http://localhost:8080/api/clientes/${id}`)
+  }
+
+  updateCliente(cliente: Cliente): Observable<any> {
+    return this.http.put<Cliente>(`http://localhost:8080/api/clientes/${cliente.id}`, cliente)
+  }
+
 }
